7f9998658cd5348b72cc0266d58e3e79e407dc98
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-08-29 23:47:33 +0530

    Hw solved

---
+++ conditional/customer.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class customer {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter your name: ");
+        String name = in.nextLine();
+        
+        System.out.println("Enter your total number of calls: ");
+        int call = in.nextInt();
+        
+        double bill = 0;
+
+        // First 50 calls are free
+        if (call <= 50) {
+            System.out.println("You have nothing to pay!");
+        } else {
+            call -= 50;  // Subtract first 50 free calls
+            
+            // Next 100 calls @ Rs 0.50 each
+            if (call > 100) {
+                bill += 100 * 0.50;
+                call -= 100;
+            } else {
+                bill += call * 0.50;  // If less than 100 calls after the first 50
+                call = 0;
+            }
+            
+            // Next 100 calls @ Rs 1.00 each
+            if (call > 100) {
+                bill += 100 * 1.00;
+                call -= 100;
+            } else if (call > 0) {
+                bill += call * 1.00;
+                call = 0;
+            }
+            
+            // Calls above 250, but less than or equal to 500 @ Rs 1.50 each
+            if (call > 0 && call <= 250) {
+                bill += call * 1.50;
+                call = 0;
+            }
+            
+            // Calls between 500 and 700 @ Rs 2.00 each
+            if (call > 0 && call <= 200) {
+                bill += call * 2.00;
+            }
+        }
+
+        System.out.println("\nCustomer Name: " + name);
+        System.out.println("Total Bill: Rs " + bill);
+
+        in.close();
+    }
+}
---
+++ conditional/product.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * product
+ */
+ class product {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double discount = 0;
+        System.out.println("Enter the product name: ");
+        String productName = in.nextLine();
+        System.out.println("Enter product price: ");
+        double productPrice = in.nextDouble();
+        if(productPrice > 1000 && productPrice  < 10000)
+        {
+            System.out.println("The product name is " + productName);
+            discount += productPrice*(5.0/100);
+            if(productPrice == 5000)
+            {
+                discount = discount + productPrice*(3.0/100);
+            }
+            System.out.println("Your discounted price is" + discount);
+        }
+        else if(productPrice >= 10000)
+        {
+            System.out.println("The product name is " + productName);
+            discount += productPrice*(7.0/100);
+            if(productPrice == 25000 || productPrice == 35000 || productPrice == 50000)
+            {
+                discount = discount + productPrice*(5.0/100);
+            }
+            System.out.println("Your discounted price is" + discount);
+        }
+
+        in.close();
+    }
+}
